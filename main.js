fdbb0c21385da1870e4ebdaa27a751f27265121c
itsmartagonzalez <alu0101254487@ull.edu.es>
2022-01-15 14:24:17 +0000

    Page works

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ import {Transformation} from './src/Transformation.js'
 import {saveAs} from 'file-saver';
 
 const file = document.querySelector('#file')
-let sat3 = {}
+const jsonInput = document.querySelector('.json-input')
+const jsonOutput = document.querySelector('.json-output')
+let sat3, sat = {}
+
 
 file.addEventListener('change', function() {
   const fr = new FileReader();
   fr.onload = function() {
-    const sat = new SAT(JSON.parse(fr.result))
-    const transformer = new Transformation(sat)
-    sat3 = transformer.transform()
+    sat = new SAT(JSON.parse(fr.result))
+    jsonInput.innerHTML = syntaxHighlight(JSON.stringify(sat, null, 2))
+    jsonOutput.innerHTML = '';
   }  
   fr.readAsText(this.files[0]);
 })
@@ -25,5 +28,33 @@ const saveFile = () => {
   saveAs(blob, filename)
 }
 
-const button = document.querySelector('button')
-button.addEventListener('click',() => saveFile())
+function syntaxHighlight(json) {
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+      var cls = 'number';
+      if (/^"/.test(match)) {
+          if (/:$/.test(match)) {
+              cls = 'key';
+          } else {
+              cls = 'string';
+          }
+      }
+      return '<span class="' + cls + '">' + match + '</span>';
+  });
+}
+
+const downloadButton = document.querySelector('.download')
+downloadButton.addEventListener('click',() => saveFile())
+
+// -------------------------------
+const transformButton = document.querySelector('.transform')
+transformButton.addEventListener('click',() => {
+  const transformer = new Transformation(sat)
+  sat3 = transformer.transform()
+  jsonOutput.innerHTML = syntaxHighlight(JSON.stringify(sat3, null, 2))
+  downloadButton.style.display = 'inline'
+})
+
+
+
+
